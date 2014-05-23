6e4189eee6af31e9e2eae0d6a1deedf799603950
Phil Wittig <pdwittig@gmail.com>
2014-05-23 14:15:25 -0700

    commented out empty spec helper test

--- spec/helpers/playlist_controller_helper_spec.rb
+++ spec/helpers/playlist_controller_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe PlaylistControllerHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe PlaylistControllerHelper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
