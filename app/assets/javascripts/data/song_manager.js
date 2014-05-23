52ed0921c34f17ddfa4bf2482dd5db4e32d09180
Phil Wittig <pdwittig@gmail.com>
2014-05-23 10:56:17 -0700

    implement song_manager.js

--- app/assets/javascripts/data/song_manager.js
+++ app/assets/javascripts/data/song_manager.js
@@ -1,7 +1,19 @@
-Data.SongManager = function() {
-
+Data.SongManager = function(searchUrl) {
+	this.searchUrl = searchUrl
+	this.songManagerSubscriber = {}
 }
 
 Data.SongManager.prototype = {
-	
+	searchSongs: function(query) {
+		ajaxOptions = { url: this.searchUrl, type: 'get', success: this.handleSongResponse.bind(this)}
+		$.ajax(ajaxOptions)
+	},
+
+	registerSongManagerSubscriber: function(subscriber) {
+		this.songManagerSubscriber = subscriber
+	},
+
+	handleSongResponse: function(songData) {
+		this.songManagerSubscriber.update(songData)
+	}
 }
