eb1bc6c52e93b9c1b9cf997b274934ea69ca394a
Phil Wittig <pdwittig@gmail.com>
2014-05-24 17:06:28 -0700

    merged studio and studio collection into one model

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
-  sModel = new StudioCollection.Model()
-  sView = new StudioCollection.View()
+  studioCollectionModel = new StudioCollection.Model()
+  studioCollectionView = new StudioCollection.View()
+  var studioModel = new Studio.Model();
+  var studioView = new Studio.View();
   
-  studioCollectionController = new StudioCollection.Controller(sModel, sView)
-  fbTest = new Data.FirebaseManager('https://trala.firebaseio.com/studioCollection', sModel)
-  sModel.registerStudioCollectionSubscriber(fbTest)
-  sModel.registerController(studioCollectionController)
-  studioCollectionController.init()
+  studioCollectionController = new StudioCollection.Controller({studioCollectionModel: studioCollectionModel,
+                                                                studioCollectionView: studioCollectionView,
+                                                                studioModel: studioModel,
+                                                                studioView: studioView
+                                                                 })
+  
+  fbTest = new Data.FirebaseManager('https://trala.firebaseio.com/studioCollection', studioCollectionModel)
+  studioCollectionModel.registerStudioCollectionSubscriber(fbTest)
+  studioCollectionModel.registerController(studioCollectionController)
+  studioCollectionController.initStudioCollection()
   fbTest.setDataTriggers()
 
 
   //Studio Setup - TODO cleanup when search is complete
   var songManager = new Data.SongManager();
   songManager.init()
-  var studioModel = new Studio.Model();
-  var studioView = new Studio.View();
-  studioController = new Studio.Controller({model: studioModel,
-  																					view: studioView,
-  																		 			songManager: songManager})
  // studioController.init(); 
 
 // App Controller Setup
@@ -27,8 +29,8 @@ var searchController = new Search.Controller()
 
 appView.registerEventDelegate(appController)
 appView.bindEvents()
-appController.registerStudioCollectionController(studioCollectionController, "loadInitial")
-appController.registerStudioController(studioController, "loadInitial")
+appController.registerStudioCollectionController(studioCollectionController, "loadInitialStudioCollection")
+appController.registerStudioController(studioCollectionController, "loadInitialStudio")
 appController.registerSearchController(searchController, "loadWidget")
   
 })
