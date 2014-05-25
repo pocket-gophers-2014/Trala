ec8a0fb80adb765b048316570f21043cfc950c6a
Lindsey Menges <lindseymenges@gmail.com>
2014-05-25 14:34:40 -0700

    adds initial capybara testing

---
+++ spec/features/user_creates_new_studio_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'User can create a new studio' do
+  scenario 'click on create room button', :js => true do
+      visit root_path
+      page.find("#create-studio").click
+      expect(page).to have_content('Your playlist')
+  end
+end
+
+
+# This test is currently breaking because the AJAX call is not going through in Firefox (which is the browser that Capybara is running in). I'm pushing it up right now and it SHOULD work, but we need to look into why stuff is breaking in Firefox
+feature 'Search renders new results' do
+  scenario 'render results after searching', :js => true do
+    visit root_path
+    page.find("#create-studio").click
+    fill_in('all_search', :with => 'Beethoven')
+    page.find("#search-submit").click
+    expect(page).to have_content('Song Id:')
+  end
+end
