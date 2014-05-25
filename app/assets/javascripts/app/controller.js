3f853ec76e9710fa460f54c9b8706e9ab850c150
AyyZee <apprentice@dbc23.local>
2014-05-24 23:16:02 -0700

    fix stuff

--- app/assets/javascripts/app/controller.js
+++ app/assets/javascripts/app/controller.js
@@ -8,28 +8,28 @@ App.Controller.prototype = {
 	loadNewStudio: function(){
 		var studioController = this.studioController.controller
 		var studioMethod = studioController[this.studioController.callbackMethod]
-		studioMethod.apply(studioController)
+		studioMethod.apply( studioController )
 
 		var searchController = this.searchController.controller
 		var searchMethod = searchController[this.searchController.callbackMethod]
-		searchMethod.apply(searchController)
+		searchMethod.apply( searchController )
 	},
 
 	loadStudioCollection: function(){
 		var studioController = this.studioCollectionController.controller
 		var studioMethod = studioController[this.studioCollectionController.callbackMethod]
-		studioMethod.apply(studioController)
+		studioMethod.apply( studioController )
 	},
 
-	registerStudioCollectionController: function(controller, callbackMethod){
-		this.studioCollectionController = {controller: controller, callbackMethod: callbackMethod}
+	registerStudioCollectionController: function( controller, callbackMethod ){
+		this.studioCollectionController = { controller: controller, callbackMethod: callbackMethod }
 	},
 
-	registerStudioController: function(controller, callbackMethod){
+	registerStudioController: function( controller, callbackMethod ){
 		this.studioController = {controller: controller, callbackMethod: callbackMethod}
 	},
 
-	registerSearchController: function(controller, callbackMethod){
-		this.searchController = {controller: controller, callbackMethod: callbackMethod}
+	registerSearchController: function( controller, callbackMethod ){
+		this.searchController = { controller: controller, callbackMethod: callbackMethod }
 	}
 }
