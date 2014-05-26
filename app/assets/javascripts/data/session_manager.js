4ff800f9ba90df6b2639b68d454f9191188b9ad2
AyyZee <apprentice@dbc23.local>
2014-05-25 17:01:15 -0700

    Adds responsive grid to search_results_template / removes albums without artwork.

---
+++ app/assets/javascripts/data/session_manager.js
@@ -0,0 +1,17 @@
+Data.SessionManager = function() {
+   // this.userID = ""
+   // this.CurrentPage = ""
+}
+
+Data.SessionManager.prototype = {
+  // setUser: function() {},
+
+  // setCurrentPage() {},
+
+  // _fetchUserId() {},
+
+  // _fetchCurrentPage() {},
+
+  // _generateRandomNumber() {}
+
+}
